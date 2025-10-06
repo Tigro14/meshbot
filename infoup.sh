f97494b26e3e1496162104e2fdb0ae6f076ff3a8
tigro <tigro@tigro.fr>
2025-10-06 14:10:38 +0000

    send json to mapserver: improve aspect link weight

--- infoup.sh
+++ infoup.sh
@@ -2,14 +2,17 @@
 
 # Script: update_info_json.sh
 
+
 # Chemin vers le fichier info.json
-JSON_FILE="info.json"
+JSON_FILE="/home/dietpi/bot/info.json"
+
+cd /home/dietpi/bot
 
 meshtastic --host 192.168.1.38 --info > $JSON_FILE
 
 python3 info_json_clean.py info.json info_clean.json
 
-mv info_clean.json info.json
+mv info_clean.json $JSON_FILE
 
 /usr/bin/scp $JSON_FILE root@100.120.148.60:/opt/WebSites/projectsend/. 
 
