a7b37c7752814b50e4921e308a1d790e3988686e
tigro <tigro@tigro.fr>
2025-10-09 02:48:12 +0000

    readme_cleanup

--- map/infoup.sh
+++ map/infoup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-# Script: update_info_json.sh
-
+# Met à jour les fichier JSON depuis le node MESH vers les cartes HTML/JS/CSS
 
 # Chemin vers le fichier info.json
 JSON_FILE="/home/dietpi/bot/map/info.json"
+JSON_LINKS_FILE="/home/dietpi/bot/map/info_neighbours.json"
 
 cd /home/dietpi/bot/map
+
+/home/dietpi/bot/map/export_neighbors.py > $JSON_LINKS_FILE
 
 meshtastic --host 192.168.1.38 --info > $JSON_FILE
 
@@ -14,6 +15,7 @@ python3 info_json_clean.py info.json info_clean.json
 
 mv info_clean.json $JSON_FILE
 
-# Envoie le JSON vers le serveur qui héberge map.html et meshlink.html
+# Envoie les JSON vers le serveur qui héberge map.html et meshlink.html
 /usr/bin/scp $JSON_FILE root@100.120.148.60:/opt/WebSites/projectsend/. 
+/usr/bin/scp $JSON_LINKS_FILE root@100.120.148.60:/opt/WebSites/projectsend/. 
 
